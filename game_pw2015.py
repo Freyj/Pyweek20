5b289e49e6f4efe7b421a83329032b91be731ddc
Freyja Mardottir <aerandil@gmail.com>
2015-08-10 13:26:35 +0200

    events for the start menu
    Buttons work !
    Nothing after that though.

--- game_pw2015.py
+++ game_pw2015.py
@@ -33,7 +33,22 @@ class Game:
                   if event.type == pygame.MOUSEBUTTONUP:
                         #for testing purposes
                         print(pygame.mouse.get_pos())
-
+                        #for the beginning, testing if the mouse
+                        #clicked on a button
+                        mouse = pygame.mouse.get_pos()
+                        #events on the starting menu
+                        if self.state == STATES[1]:
+                            rect_new = pygame.Rect((260,210),(260,60))
+                            rect_high = pygame.Rect((260,300),(260,60))
+                            rect_quit = pygame.Rect((260,390),(260,60))
+                            if pygame.Rect.collidepoint(rect_new, mouse):
+                                game_state = STATES[2]
+                            elif pygame.Rect.collidepoint(rect_high,mouse):
+                                game_state = STATES[3]
+                            elif pygame.Rect.collidepoint(rect_quit,mouse):
+                                self.started = False
+                        #events on the highscore menu
+                        #events on the game screen
                 if self.state == STATES[1]:
                     #make pictures used & blit them
                     self.background = loadImg('img','background.jpg')
